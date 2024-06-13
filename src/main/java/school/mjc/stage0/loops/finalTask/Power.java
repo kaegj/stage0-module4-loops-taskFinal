75f3e6db4267da875464482153367c2bb4453f74
kaegan <kkiefer4@student.umgc.edu>
2024-06-13 14:57:25 +0900

    Submission 2

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.finalTask;
 public class Power {
     public void printPower(int numberToPrint, int power) {
         int total = numberToPrint;
-        for (int i = 1; i <= power; i++) {
+        for (int i = 1; i < power; i++) {
             total *= numberToPrint;
         }
         System.out.println(total);
