c638b4bef579675f900a73e7a4f90918778e594c
kaegan <kkiefer4@student.umgc.edu>
2024-06-13 15:01:14 +0900

    Submission 5

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
-        for(int i = 1; i <= 12; i++) {
+        for(int i = 1; i <= 10; i++) {
             System.out.println(i + " x " + numberTableToPrint + " = " + (numberTableToPrint * i));
         }
     }
